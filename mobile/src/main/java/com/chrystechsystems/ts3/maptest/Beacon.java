eab1e1b22477c5cf19422de127bd7393039aeda1
Nova Reaper <chrystechsystems@gmail.com>
2018-10-21 20:03:02 -0500

    Fixed a few tools and such, instrumented tests should now be set up well enough for local verification that server data gather works

--- mobile/src/main/java/com/chrystechsystems/ts3/maptest/Beacon.java
+++ mobile/src/main/java/com/chrystechsystems/ts3/maptest/Beacon.java
@@ -19,10 +19,10 @@ public class Beacon implements Location {
     private int id, room, floor;
     private String name, lat, lon, bldg;
 
-    public Beacon(int id){
+    /*public Beacon(int id){
         System.out.println("new Beacon");
         getBeacon(id);
-    }
+    }*/
     public Beacon(int id, String bldg, int room, String name, String lat, String lon, int floor){
         this.id = id;
         this.bldg = bldg;
@@ -67,15 +67,16 @@ public class Beacon implements Location {
     }
 
     public static Beacon systemBeacon = null;
-    private Beacon getBeacon(int id) {
-        System.out.println("Getting beacon with id: " + id);
-        final int id3 = id;
-
+    public static Beacon getBeacon(String bldg, int rm) {
+        //System.out.println("Getting beacon with id: " + id);
+        //final int id3 = id;
+        final String bldgTop = bldg;
+        final int rmTop = rm;
 
         AsyncTask.execute(new Runnable() {
             //final int id2 = id3;
-            final String building = InitialDisplay.building;
-            final int room = InitialDisplay.room;
+            final String building = bldgTop;
+            final int room = rmTop;
             @Override
             public void run() {
                 System.out.println("In ASYNC Run Loop");
@@ -105,11 +106,10 @@ public class Beacon implements Location {
                         systemBeacon = new Beacon(jsonReader);
                     } else {
                         // Error handling code goes here
+                        System.err.println("Server request failed");
                     }
 
 
-                } catch (MalformedURLException MUE) {
-                    MUE.printStackTrace();
                 } catch (IOException IOE) {
                     IOE.printStackTrace();
                 }
@@ -143,7 +143,7 @@ public class Beacon implements Location {
     public String getLocName(){return name;}
     public String getLatitude(){return lat;}
     public String getLongitude(){return lon;}
-    public LatLng getCoordinates(){if(lat != null && lon != null)return new LatLng(Double.parseDouble(lat),Double.parseDouble(lon));else return null;}
+    public LatLng getCoordinates(){if(lat != null && lon != null)return new LatLng(Double.parseDouble(getLatitude()),Double.parseDouble(getLongitude()));else return null;}
     public int getFloor(){return floor;}
     public Beacon getCurrentBeacon(){return currentBeacon;}
     public Beacon getTargetBeacon(){return targetBeacon;}
