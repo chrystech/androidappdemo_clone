507d3fcd0362db64b40dc6e0b57209f0c8713de5
Nova Reaper <chrystechsystems@gmail.com>
2018-10-27 21:29:20 -0500

    IndoorTracking now automatically is selected upon conditions, currently arrow "Attempts" to point north
    Beginning of attempt to point towards target rather than north.

--- mobile/src/main/java/com/chrystechsystems/ts3/maptest/IndoorTracking.java
+++ mobile/src/main/java/com/chrystechsystems/ts3/maptest/IndoorTracking.java
@@ -1,22 +1,36 @@
 package com.chrystechsystems.ts3.maptest;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
-public class IndoorTracking extends AppCompatActivity {
+import com.google.android.gms.maps.model.LatLng;
 
+public class IndoorTracking extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private ImageView directionImage;
+    private float lastHeading = 0f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indoor_tracking);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        directionImage = findViewById(R.id.directionImage);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,5 +39,50 @@ public class IndoorTracking extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public void onResume(){
+        super.onResume();
+        // for the system's orientation sensor registered listeners
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),SensorManager.SENSOR_STATUS_ACCURACY_HIGH);/*.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR,//Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);*/
 
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        System.err.println("Trying to get to this heading: ");
+        Beacon target = Beacon.getBeacon(InitialDisplay.building, InitialDisplay.room);
+        LatLng coordinates = target.getCoordinates();
+        int currentBearing = Math.round(event.values[0]);
+        System.err.println(coordinates);
+        //System.err.println("event.values: " + event.values);
+        System.err.print("event.values: ");
+        for(int i = 0; i < event.values.length; i++){
+            System.err.print(event.values[i] + " | ");
+        }
+        System.err.println();
+        System.err.println("Bearing: " + Math.round((event.values[0])));
+        System.out.println("LAST LOCATION KNOWN: " + MapsActivity.lastRecordedLocation);
+        System.out.println("TARGET LOCATION -->: " + coordinates);
+        RotateAnimation ra = new RotateAnimation(lastHeading,-currentBearing,Animation.RELATIVE_TO_SELF, 0.5f,
+
+                Animation.RELATIVE_TO_SELF,
+
+                0.5f);
+        ra.setDuration(210);
+        ra.setFillAfter(true);
+        directionImage.startAnimation(ra);
+        lastHeading = -Math.round((event.values[0]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
