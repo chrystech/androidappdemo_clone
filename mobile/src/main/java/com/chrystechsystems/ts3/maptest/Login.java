4a4012090967435a26bae086251aed313b42252e
alc73 <alc73@students.uwf.edu>
2018-11-04 21:51:53 -0600

    Added a register button to login page, fixed GMap and random floating text bug, created preliminal registration page.

--- mobile/src/main/java/com/chrystechsystems/ts3/maptest/Login.java
+++ mobile/src/main/java/com/chrystechsystems/ts3/maptest/Login.java
@@ -22,24 +22,34 @@ public class Login extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Code for guest button: just skips to initial display (screen where you enter bldg and rm info)
+        // Code for guest button: skips to initial display (screen where you enter bldg and rm info)
         Button guestButton = findViewById(R.id.guestButton);
         guestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    startActivity(new Intent(Login.this,InitialDisplay.class));
-                }catch(Exception E){
-                    Snackbar.make(view,"Who knows what happened!",Snackbar.LENGTH_LONG).setAction("Action",null).show();
-                }
+                startActivity(new Intent(Login.this,InitialDisplay.class));
             }
         });
 
+        // Code for register button: sends user to registration page
+        Button registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this,Registration.class));
+            }
+        });
+
+        // When mail icon (floating action button/fab) is clicked on the login page this code is executed: code should check if passed-in credentials —
+        // from username and password textviews (text input fields in activity_login.xml/content_login.xml) are valid by querying API
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //username = ((TextView) findViewById(R.id.username)).getText().toString();
+                username = ((TextView) findViewById(R.id.username)).getText().toString();
+                password = ((TextView) findViewById(R.id.password)).getText().toString();
+
+                // TODO query API asking whether username and password pair exist
 
                 Snackbar.make(view, "Credentials do not exist", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
